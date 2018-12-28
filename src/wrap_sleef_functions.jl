5d3d5f11abb72f4f3959454b815da08d7f3f3afc
Chris Elrod <elrodc@gmail.com>
2018-12-28 13:16:06 -0600

    Now support variably sized input vectors.

--- src/wrap_sleef_functions.jl
+++ src/wrap_sleef_functions.jl
@@ -71,26 +71,87 @@ const BINARY_IN_FUNCTIONS = [
 for (vectype, func_suffix, suffix) ∈ SIZES
     for (mod,SLEEF_name,Julia_name,accuracy) ∈ UNARY_OUT_FUNCTIONS
         func_name = QuoteNode(Symbol(PREFIX, SLEEF_name, func_suffix, accuracy, suffix))
-        @eval function ($Julia_name)(a::($vectype))
+        @eval @inline function ($Julia_name)(a::($vectype))
             ccall(($func_name, libsleef), $vectype, ($vectype,), a)
         end
     end
 
     for (mod,SLEEF_name,Julia_name,accuracy) ∈ BINARY_OUT_FUNCTIONS
         func_name = QuoteNode(Symbol(PREFIX, SLEEF_name, func_suffix, accuracy, suffix))
-        @eval function ($Julia_name)(a::($vectype))
+        @eval @inline function ($Julia_name)(a::($vectype))
             ccall(($func_name, libsleef), $(NEXT[vectype]), ($vectype,), a)
         end
     end
 
     for (mod,SLEEF_name,Julia_name,accuracy) ∈ BINARY_IN_FUNCTIONS
         func_name = QuoteNode(Symbol(PREFIX, SLEEF_name, func_suffix, accuracy, suffix))
-        @eval function ($Julia_name)(a::($vectype), b::($vectype))
+        @eval @inline function ($Julia_name)(a::($vectype), b::($vectype))
             ccall(($func_name, libsleef), $vectype, ($vectype,$vectype), a, b)
         end
     end
 end
 
+function split_quote(func, args, outs, W, T, ::Type{VT}) where VT
+    R = W
+    current_vec_size = REGISTER_SIZE ÷ sizeof(T)
+    repetitions = Pair{Int,Int}[]
+    while R != 0
+        N, R = divrem(W, current_vec_size)
+        push!(repetitions, N => current_vec_size)
+        current_vec_size >>= 1
+    end
+    eargs = [gensym(arg) for arg ∈ args]
+    outputs = [gensym(out) for out ∈ outs]
+    finaloutputs = [Expr(:tuple) for i ∈ eachindex(outputs)]
+    q = quote
+        $(Expr(:meta, :inline))
+    end
+    for i ∈ eachindex(eargs)
+        push!(q.args, :($(eargs[i]) = extract_data($(args[i]))))
+    end
+    ind = 0
+    for (reps, vec_size) ∈ repetitions
+        # arg_quote = quote end
+        for k ∈ 1:reps
+            targs = Vector{Symbol}(undef, length(args))
+            for (i,arg) ∈ enumerate(eargs)
+                targ = gensym(:arg)
+                targs[i] = targ
+                push!(q.args, :( $targ = $(Expr(:tuple, [:($arg[$(ind + j)]) for j ∈ 1:vec_size]...)) ))
+            end
+            touts = Vector{Symbol}(undef, length(outputs))
+            for (i,out) ∈ enumerate(outputs)
+                tout = gensym(out)
+                touts[i] = tout
+                texpr = finaloutputs[i]
+                for j ∈ 1:vec_size
+                    push!(texpr.args, :(($tout)[$j]) )
+                end
+            end
+            ind += vec_size
+            if length(outputs) == 1
+                push!(q.args, :($(touts[1]) =  $(Expr(:call, func, targs...)) ))
+            else
+                push!(q.args, :($(Expr(:tuple, touts...)) =  $(Expr(:call, func, targs...)) ))
+            end
+        end
+    end
+    struct_wrap = VT <: VectorizationBase.AbstractStructVec
+    if length(outputs) == 1
+        if struct_wrap
+            push!(q.args, :(SIMDPirates.SVec($(finaloutputs[1]))))
+        else
+            push!(q.args, finaloutputs[1])
+        end
+    else
+        if struct_wrap
+            push!(q.args, Expr(:tuple, [:(SIMDPirates.SVec($fo)) for fo ∈ finaloutputs]...))
+        else
+            push!(q.args, Expr(:tuple, finaloutputs...))
+        end
+    end
+    q
+end
 
 for (mod,SLEEF_name,Julia_name,accuracy) ∈ UNARY_OUT_FUNCTIONS
     func_name = :($mod.$Julia_name)
@@ -105,6 +166,14 @@ for (mod,SLEEF_name,Julia_name,accuracy) ∈ UNARY_OUT_FUNCTIONS
         @eval @inline $func_name(a::AbstractStructVec{$W,Float64}) = SVec($sleef_name(extract_data(a)))
         if mod != :SLEEFwrap
             @eval @inline $sleef_name(a::AbstractStructVec{$W,Float64}) = SVec($sleef_name(extract_data(a)))
+        end
+    end
+    @eval @generated function $sleef_name(a::AbstractSIMDVector{W,T}) where {W,T}
+        split_quote($sleef_name, (:a, ), (:c,), W, T, a)
+    end
+    if mod != :SLEEFwrap
+        @eval @generated function $func_name(a::AbstractSIMDVector{W,T}) where {W,T}
+            split_quote($sleef_name, (:a, ), (:c,), W, T, a)
         end
     end
 end
@@ -122,6 +191,14 @@ for (mod,SLEEF_name,Julia_name,accuracy) ∈ BINARY_OUT_FUNCTIONS
         @eval @inline $func_name(a::AbstractStructVec{$W,Float64}) = SVec($sleef_name(extract_data(a)))
         if mod != :SLEEFwrap
             @eval @inline $sleef_name(a::AbstractStructVec{$W,Float64}) = SVec($sleef_name(extract_data(a)))
+        end
+    end
+    @eval @generated function $sleef_name(a::AbstractSIMDVector{W,T}) where {W,T}
+        split_quote($sleef_name, (:a, ), (:b, :c,), W, T, a)
+    end
+    if mod != :SLEEFwrap
+        @eval @generated function $func_name(a::AbstractSIMDVector{W,T}) where {W,T}
+            split_quote($sleef_name, (:a, ), (:b, :c,), W, T, a)
         end
     end
 end
@@ -151,6 +228,15 @@ for (mod,SLEEF_name,Julia_name,accuracy) ∈ BINARY_IN_FUNCTIONS[2:end]
             @eval @inline function $sleef_name(a::AbstractStructVec{$W,Float64}, b::AbstractStructVec{$W,Float64})
                 Vec($sleef_name(extract_data(a),extract_data(b)))
             end
+        end
+    end
+
+    @eval @generated function $sleef_name(a::V, b::V) where {W,T, V <: AbstractSIMDVector{W,T}}
+        split_quote($sleef_name, (:a, :b), (:c,), W, T, V)
+    end
+    if mod != :SLEEFwrap
+        @eval @generated function $func_name(a::V, b::V) where {W,T, V <: AbstractSIMDVector{W,T}}
+            split_quote($sleef_name, (:a, :b), (:c,), W, T, V)
         end
     end
 end
