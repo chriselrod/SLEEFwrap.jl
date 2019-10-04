f6d42e8a86cb72f686720bab42074b5b4d745c2b
Chris Elrod <elrodc@gmail.com>
2019-10-04 00:39:15 -0500

    AbstractStructVec moved to VectorizationBase.

--- src/SLEEFwrap.jl
+++ src/SLEEFwrap.jl
@@ -2,7 +2,7 @@ module SLEEFwrap
 
 using SIMDPirates, VectorizationBase, SpecialFunctions, Base.FastMath, LoopVectorization
 import MacroTools: @capture, postwalk, prewalk
-using SIMDPirates: Vec
+using VectorizationBase: AbstractStructVec
 
 export @sleef, @fastsleef, @spirate, @spiratef, @vectorize, @cvectorize
 
